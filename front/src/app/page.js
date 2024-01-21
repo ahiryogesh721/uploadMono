a9b9fafd1d28e393d52e7850066431a583bff6ae
mac yog <ahiryogesh721@gmail.com>
2024-01-21 21:26:14 +0530

    addeed

--- front/src/app/page.js
+++ front/src/app/page.js
@@ -5,8 +5,8 @@ import BarC from "@/components/barX";
 import LineM from "@/components/lineM";
 import L2 from "@/components/L2";
 export default function Home() {
-  let to = 3200,
-    from = 3400;
+  let to = 4600,
+    from = 4800;
   return (
     <div className="mt-32 flex flex-row md:mt-0 md:flex-col ">
       <div>
--- front/src/components/L2.jsx
+++ front/src/components/L2.jsx
@@ -259,14 +259,13 @@ export default function L1({ to, from, c1, c2 }) {
     let box2 = [];
     let alow1 = true;
     chartArr.forEach((x, i) => {
-      if (alow1 && x.D2 > 0) {
-        let obj = {};
+      if (alow1 && x.D2 > 6) {
+        /* let obj = {};
         let condition = true;
         let I = 1;
         obj.IP = x.I;
         obj.MS = [];
         while (condition) {
-          console.log(x.I, I);
           let nextInVal = chartArr[i + I];
           if (nextInVal?.D2 === -1) {
             obj.MS.push({ i: nextInVal?.I, val: nextInVal?.D2 });
@@ -275,7 +274,10 @@ export default function L1({ to, from, c1, c2 }) {
             condition = false;
           }
           I++;
-        }
+        } */
+        chartArr[i + 1]?.D2 === -1
+          ? box2.push({ i: x.I, val: 1 })
+          : box2.push({ i: x.I, val: 0 });
         alow1 = false;
       } else if (x.D2 === 0) {
         alow1 = true;
@@ -283,15 +285,17 @@ export default function L1({ to, from, c1, c2 }) {
     });
     console.log(box2);
 
-    /* box2.forEach((x, i) => {
-      if (
-        x.MS.length === 0 &&
-        box2[i - 1]?.MS.length === 0 &&
-        box2[i - 2]?.MS.length === 0
-      ) {
-        console.log(x.IP);
+    /* let box3 = [];
+    let alllow = true;
+    box2.forEach((x, i) => {
+      if (x.MS.length !== 0 && alllow) {
+        box3.push({ i: x.IP, val: box2[i + 1]?.MS.length });
+        alllow = false;
+      } else if (x.MS.length === 0) {
+        alllow = true;
       }
-    }); */
+    });
+    console.log(box3); */
   };
 
   const box5Fun = () => {
@@ -338,7 +342,7 @@ export default function L1({ to, from, c1, c2 }) {
     let allow = true;
     let box3 = [];
     chartArr.forEach((x, i) => {
-      if (x.D3 === 2 || x.D3 === 3) {
+      if (x.D3 > 9) {
         chartArr[i + 1]?.D3 === -1
           ? box3.push({ i: x.I, val: 1 })
           : box3.push({ i: x.I, val: 0 });
@@ -355,7 +359,7 @@ export default function L1({ to, from, c1, c2 }) {
     //cheker();
     //box2Fun();
     //box3Fun();
-    box5Fun();
+    //box5Fun();
   }, [chartArr.length]);
 
   useEffect(() => {
