396b256a3ccf561373cc09d0f2745a496ec205c0
mac yog <ahiryogesh721@gmail.com>
2024-02-23 04:51:10 +0530

    done

--- back/middleware/corsMiddle.js
+++
@@ -1,9 +0,0 @@
-const credentials = (req, res, next) => {
-  const origin = req.headers.origin;
-  if (true) {
-    res.header("Access-Control-Allow-Credentials", true);
-  }
-  next();
-};
-
-module.exports = credentials;
