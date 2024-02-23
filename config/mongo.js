eadd72a05b00089bc4503a89d6d8e479b9845bce
mac yog <ahiryogesh721@gmail.com>
2024-02-23 13:21:29 +0530

    setel1

---
+++ config/mongo.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const conectMongo = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://ahiryogesh:ahiryogesh721@tocxic.fhprltu.mongodb.net/"
+    );
+    console.log("database is conencted");
+  } catch (error) {
+    console.log("couldnt able to conect databse");
+  }
+};
+
+module.exports = conectMongo;
