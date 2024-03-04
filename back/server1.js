38056bd580025d687d548e0eadb5d0644722b6a7
mac yog <ahiryogesh721@gmail.com>
2024-03-05 01:40:20 +0530

    ++

--- back/server1.js
+++ back/server1.js
@@ -4,8 +4,11 @@ const { Server } = require("socket.io");
 const accountSid = "AC1365e0479e0ea18054b3f69f3b441e0f";
 const authToken = "5412c9f13c254c91a01e8e15eb256fc0";
 const client = require("twilio")(accountSid, authToken);
+const json = require("body-parser").json();
 
 const PORT = 3100;
+
+app.use(json);
 
 const appL = app.listen(PORT, () => {
   console.log(`socket server is runing on PORT:${PORT}`);
