810f5fce6a8265f5142fc869f88caa96a80cbbcd
mac yog <ahiryogesh721@gmail.com>
2024-03-05 13:30:37 +0530

    changes in bob

--- back/bob.js
+++ back/bob.js
@@ -24,8 +24,40 @@ const itrater = () => {
       "#games_page > div.crash.games-container__game > div.crash__wrap.crash__wrap--main > div.crash__game.crash-game > div.crash-game__timeline > svg > g:nth-child(5) > text"
     )
     .textContent.trim();
-  if (playersGetsSpanVal === "0") {
-    fetch("http://localhost:3500/post", {
+
+  let adj = +xGVal.slice(0, xGVal.length - 1);
+  console.log(adj);
+
+  let userTag1 = document.querySelector(
+    "#games_page > div.crash.games-container__game > div.crash-players-bets.crash__wrap.crash__wrap--left > div.crash-players-bets__results.crash-results > div > table > tbody > tr:nth-child(1)"
+  );
+
+  let userTag2 = document.querySelector(
+    "#games_page > div.crash.games-container__game > div.crash-players-bets.crash__wrap.crash__wrap--left > div.crash-players-bets__results.crash-results > div > table > tbody > tr:nth-child(2)"
+  );
+
+  let userTag3 = document.querySelector(
+    "#games_page > div.crash.games-container__game > div.crash-players-bets.crash__wrap.crash__wrap--left > div.crash-players-bets__results.crash-results > div > table > tbody > tr:nth-child(3)"
+  );
+
+  let userTag4 = document.querySelector(
+    "#games_page > div.crash.games-container__game > div.crash-players-bets.crash__wrap.crash__wrap--left > div.crash-players-bets__results.crash-results > div > table > tbody > tr:nth-child(4)"
+  );
+
+  let userTag5 = document.querySelector(
+    "#games_page > div.crash.games-container__game > div.crash-players-bets.crash__wrap.crash__wrap--left > div.crash-players-bets__results.crash-results > div > table > tbody > tr:nth-child(5)"
+  );
+
+  if (
+    playersGetsSpanVal === "0" ||
+    (adj < 2 &&
+      (userTag1.classList.contains("crash-results__row--loss") ||
+        userTag2.classList.contains("crash-results__row--loss") ||
+        userTag3.classList.contains("crash-results__row--loss") ||
+        userTag4.classList.contains("crash-results__row--loss") ||
+        userTag5.classList.contains("crash-results__row--loss")))
+  ) {
+    fetch("https://backend-fdok.onrender.com/post", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
